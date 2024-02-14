27f96685b63b9f00c4a033b6f8f6540c93f0eae3
BNdungu <bryannganga909@gmail.com>
2024-02-14 22:42:01 +0300

    nesting components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@
 import ReactDOM from "react-dom";
 
 function Greetings() {
-  return <h1>afdshew hdsaf</h1>;
+  return <div>
+    <Name/>
+    <Profession/>
+  </div>
+}
+
+const Name = () => <p>My name is John Doe</p>;
+
+const Profession = () => {
+    return <p>I'm a Software Dev</p>;
 }
 
 ReactDOM.render(<Greetings />, document.getElementById("root"));
