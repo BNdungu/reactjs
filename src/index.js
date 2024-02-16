d546b37dc22ba1695678ea6af084b6633690b39c
BNdungu <bryannganga909@gmail.com>
2024-02-16 21:28:47 +0300

    props

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import "./style.css";
 
+const firstBook = {
+  title : "Madness: Race and Insanity in a Jim Crow Asylum",
+  author : "Antonia Hylton",
+  img : "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
+}
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book title = {firstBook.title} img = {firstBook.img} author={firstBook.author}/>
       <Book />
       <Book />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
     <section className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
-        alt=""
-      />
-      <h1>Madness: Race and Insanity in a Jim Crow Asylum</h1>
-      <h3>Antonia Hylton</h3>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h3>{props.author}</h3>
     </section>
   );
 };
