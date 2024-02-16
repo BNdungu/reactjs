87f38af2f4ca13c24cab60808a89ac7861ca35a7
BNdungu <bryannganga909@gmail.com>
2024-02-16 22:58:50 +0300

    map through array && add key value

--- src/index.js
+++ src/index.js
@@ -3,54 +3,42 @@ import ReactDOM from "react-dom";
 
 import "./style.css";
 
-const firstBook = {
-  title: "Madness: Race and Insanity in a Jim Crow Asylum",
-  author: "Antonia Hylton",
-  img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
-};
+const books = [
+  {
+    _id: 1,
+    title: "Madness: Race and Insanity in a Jim Crow Asylum",
+    author: "Antonia Hylton",
+    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+  },
+  {
+    _id: 2,
+    title: "Madness: Race and Insanity in a Jim Crow Asylum",
+    author: "San Diego",
+    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+  },
+  {
+    _id: 3,
+    title: "lorem ipsum and the first Angel",
+    author: "Hamilton Mega",
+    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+  },
+];
 
-const secondBook = {
-  title: "Madness: Race and Insanity in a Jim Crow Asylum",
-  author: "San Diego",
-  img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
-};
-
-const thirdBook = {
-  title: "lorem ipsum and the first Angel",
-  author: "Hamilton Mega",
-  img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
-};
 
 function BookList() {
+
   return (
-    <section className="booklist">
-      <Book
-        title={firstBook.title}
-        img={firstBook.img}
-        author={firstBook.author}
-      >
-        <p className="children">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In quaerat,
-          deleniti, minus maiores quam voluptatum non voluptatem eaque similique
-          autem officiis porro. Quidem inventore in esse atque fugit mollitia
-          iusto.
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
-      <Book
-        title={thirdBook.title}
-        img={thirdBook.img}
-        author={thirdBook.author}
-      />
+    <section className='booklist'>
+      {books.map((item) => {
+        return <Book key = {item._id} book={item}/>
+      })}
     </section>
-  );
+  )
 }
 
-const Book = ({ img, author, title, children}) => {
+const Book = (props) => {
+  console.log(props)
+  const {img,author,title,children} = props.book
   return (
     <section className="book">
       <img src={img} alt="" />
