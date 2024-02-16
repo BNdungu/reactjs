ffcea9182cbc114d0b5fc39a24887a28ee02675b
BNdungu <bryannganga909@gmail.com>
2024-02-16 14:55:50 +0300

    nesting components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 // import React from "react";
 import ReactDOM from "react-dom";
 
-function Greetings() {
-  return <div>
-    <Name/>
-    <Profession/>
-  </div>
+function BookList() {
+  return <section>
+    <Book/>
+  </section>
 }
 
-const Name = () => <p>My name is John Doe</p>;
-
-const Profession = () => {
-    return <p>I'm a Software Dev</p>;
+const Book = () => {
+  return (
+    <section>
+      <Image/>
+      <Title/>
+      <Author/>
+    </section>
+  );
 }
 
-ReactDOM.render(<Greetings />, document.getElementById("root"));
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
+      alt=""
+    />
+  );
+}
+
+const Title = () => {
+  return <h1>Madness: Race and Insanity in a Jim Crow Asylum</h1>
+}
+
+const Author = () => {
+  return <h3>Antonia Hylton</h3>;
+}
+ReactDOM.render(<BookList />, document.getElementById("root"));
