87bf695460ad96056f00ab17af66d8956f270172
BNdungu <bryannganga909@gmail.com>
2024-02-16 21:41:13 +0300

    adv props

--- src/index.js
+++ src/index.js
@@ -9,12 +9,36 @@ const firstBook = {
   img : "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
 }
 
+const secondBook = {
+  title: "Madness: Race and Insanity in a Jim Crow Asylum",
+  author: "San Diego",
+  img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+};
+
+const thirdBook = {
+  title: "lorem ipsum and the first Angel",
+  author: "Hamilton Mega",
+  img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book title = {firstBook.title} img = {firstBook.img} author={firstBook.author}/>
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        img={firstBook.img}
+        author={firstBook.author}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+      <Book
+        title={thirdBook.title}
+        img={thirdBook.img}
+        author={thirdBook.author}
+      />
     </section>
   );
 }
