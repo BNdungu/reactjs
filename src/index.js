42fadd90f8e1ba425163de72341b1db0f35af736
BNdungu <bryannganga909@gmail.com>
2024-02-16 15:30:47 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import React from "react";
 import ReactDOM from "react-dom";
 
-import './style.css'
+import "./style.css";
 
 function BookList() {
   return (
     <section className="booklist">
       <Book />
       <Book />
-      <Book/>
+      <Book />
     </section>
   );
 }
@@ -16,27 +16,14 @@ function BookList() {
 const Book = () => {
   return (
     <section className="book">
-      <Image />
-      <Title/>
-      <Author/>
+      <img
+        src="https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
+        alt=""
+      />
+      <h1>Madness: Race and Insanity in a Jim Crow Asylum</h1>
+      <h3>Antonia Hylton</h3>
     </section>
   );
-}
+};
 
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg"
-      alt=""
-    />
-  );
-}
-
-const Title = () => {
-  return <h1>Madness: Race and Insanity in a Jim Crow Asylum</h1>
-}
-
-const Author = () => {
-  return <h3>Antonia Hylton</h3>;
-}
 ReactDOM.render(<BookList />, document.getElementById("root"));
