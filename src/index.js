ec81f2ce18590e34db6b590d2756e166571ac18d
BNdungu <bryannganga909@gmail.com>
2024-02-17 10:26:27 +0300

    resources

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ const books = [
     _id: 1,
     title: "Madness: Race and Insanity in a Jim Crow Asylum",
     author: "Antonia Hylton",
-    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+    img: "https://m.media-amazon.com/images/I/81kZ3Gl3WKL._AC_UY218_.jpg",
   },
   {
     _id: 2,
-    title: "Madness: Race and Insanity in a Jim Crow Asylum",
-    author: "San Diego",
-    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: " Amelia Hepworth and Tim Warnes",
+    img: "https://m.media-amazon.com/images/I/81mpSoJzv4L._AC_UY218_.jpg",
   },
   {
     _id: 3,
-    title: "lorem ipsum and the first Angel",
-    author: "Hamilton Mega",
-    img: "https://m.media-amazon.com/images/I/51NABu3WrhL._SY445_SX342_.jpg",
+    title: "KIDS PREFERRED Guess How Much I Love You , 9 inches (96784)",
+    author: "Nutbrown Hare Bean Bag Plush",
+    img: "https://m.media-amazon.com/images/I/91Fjx9MziHL._AC_UY218_.jpg",
   },
 ];
 
