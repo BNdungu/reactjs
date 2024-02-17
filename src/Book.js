50cdc2c57475718c35eff35a05320682baffd5e6
BNdungu <bryannganga909@gmail.com>
2024-02-17 11:29:08 +0300

    eport && import Book

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const Book = () => {
-    return (
-        <div>
+const Book = (props) => {
+  const { img, author, title, children } = props;
 
-        </div>
-    )
-}
+  const clickHandler = () => {
+    alert("hello world");
+  };
+
+  const complexExample = () => {
+    console.log(author);
+  };
+
+  return (
+    <section
+      className="book"
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h3>{author}</h3>
+      <button type="button" onClick={clickHandler}>
+        button
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        complex example
+      </button>
+    </section>
+  );
+};
 
 export default Book
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./style.css";
 import { books } from "./books";
+import Book from './Book'
 
 function BookList() {
   return (
@@ -13,36 +14,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, author, title, children } = props;
-
-  const clickHandler = () => {
-    alert("hello world");
-  };
-
-  const complexExample = () => {
-    console.log(author);
-  };
-
-  return (
-    <section
-      className="book"
-      onMouseOver={() => {
-        console.log(author);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h3>{author}</h3>
-      <button type="button" onClick={clickHandler}>
-        button
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        complex example
-      </button>
-    </section>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
