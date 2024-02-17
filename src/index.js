e89ba9cdf048e807af1ffa23ec99006585d863b9
BNdungu <bryannganga909@gmail.com>
2024-02-17 11:22:23 +0300

    import an external module file j

--- src/index.js
+++ src/index.js
@@ -2,60 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./style.css";
-
-const books = [
-  {
-    _id: 1,
-    title: "Madness: Race and Insanity in a Jim Crow Asylum",
-    author: "Antonia Hylton",
-    img: "https://m.media-amazon.com/images/I/81kZ3Gl3WKL._AC_UY218_.jpg",
-  },
-  {
-    _id: 2,
-    title: "I Love You to the Moon and Back",
-    author: " Amelia Hepworth and Tim Warnes",
-    img: "https://m.media-amazon.com/images/I/81mpSoJzv4L._AC_UY218_.jpg",
-  },
-  {
-    _id: 3,
-    title: "KIDS PREFERRED Guess How Much I Love You , 9 inches (96784)",
-    author: "Nutbrown Hare Bean Bag Plush",
-    img: "https://m.media-amazon.com/images/I/91Fjx9MziHL._AC_UY218_.jpg",
-  },
-];
-
+import { books } from "./books";
 
 function BookList() {
-
   return (
-    <section className='booklist'>
+    <section className="booklist">
       {books.map((item) => {
-        return <Book key = {item._id} {...item}/>
+        return <Book key={item._id} {...item} />;
       })}
     </section>
-  )
+  );
 }
 
 const Book = (props) => {
-  const {img,author,title,children} = props
+  const { img, author, title, children } = props;
 
   const clickHandler = () => {
-    alert('hello world')
-  }
+    alert("hello world");
+  };
 
   const complexExample = () => {
     console.log(author);
-  }
+  };
 
   return (
-    <section className="book" onMouseOver = {() => {
-      console.log(author)
-    }}>
+    <section
+      className="book"
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
       <img src={img} alt="" />
-      <h1 onClick = {() => console.log(title)}>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h3>{author}</h3>
-      <button type="button" onClick={clickHandler}>button</button>
-      <button type="button" onClick = {() => complexExample(author)}>complex example</button>
+      <button type="button" onClick={clickHandler}>
+        button
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        complex example
+      </button>
     </section>
   );
 };
