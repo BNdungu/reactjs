776792e807354b16c5bea3ea824bced8a27251ae
BNdungu <bryannganga909@gmail.com>
2024-02-17 11:23:03 +0300

    export data array

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    _id: 1,
+    title: "Madness: Race and Insanity in a Jim Crow Asylum",
+    author: "Antonia Hylton",
+    img: "https://m.media-amazon.com/images/I/81kZ3Gl3WKL._AC_UY218_.jpg",
+  },
+  {
+    _id: 2,
+    title: "I Love You to the Moon and Back",
+    author: " Amelia Hepworth and Tim Warnes",
+    img: "https://m.media-amazon.com/images/I/81mpSoJzv4L._AC_UY218_.jpg",
+  },
+  {
+    _id: 3,
+    title: "KIDS PREFERRED Guess How Much I Love You , 9 inches (96784)",
+    author: "Nutbrown Hare Bean Bag Plush",
+    img: "https://m.media-amazon.com/images/I/91Fjx9MziHL._AC_UY218_.jpg",
+  },
+];
