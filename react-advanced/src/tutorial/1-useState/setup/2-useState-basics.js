4482144dcf7193e6e3165f04f52663966295a181
BNdungu <bryannganga909@gmail.com>
2024-02-18 04:41:28 +0300

    useState function

--- react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const handleClick = () => {
+    setText('hello World')
+  }
+  const [text, setText] = useState('Random Title')
+  return <React.Fragment> 
+    <h1>{text}</h1>
+    <button type="button" className='btn' onClick={handleClick} >Change Title</button>
+  </React.Fragment>;
 };
 
 export default UseStateBasics;
