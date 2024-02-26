64f49104feef2bec08d7b312c34cc40d29590bd7
Ndung'u <89093672+BNdungu@users.noreply.github.com>
2024-02-26 18:24:04 +0000

    Update Book.js

--- hello-world/src/Book.js
+++ hello-world/src/Book.js
@@ -24,7 +24,7 @@ const Book = (props) => {
       <button type="button" onClick={clickHandler}>
         button
       </button>
-      <button type="button" onClick={() => complexExample(author)}>
+      <button type="button" onClick={() => complexExample(author)}> 
         complex example
       </button>
     </section>
